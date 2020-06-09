0b012a09b71696c208170d65cd95e32688ef1a37
gulzar <ali.gulzar@tuni.fi>
2020-06-09 13:47:15 +0500

    Add statistics header

--- screens/StatisticScreen.js
+++ screens/StatisticScreen.js
@@ -1,12 +1,17 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
 export default function StatisticScreen() {
   return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Statistics</Text>
+      </View>
+      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
 
-    </ScrollView>
+      </ScrollView>
+    </View>
   );
 }
 
@@ -14,6 +19,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  headerContainer: {
+    alignItems: 'center',
+    marginTop: 30
+  },
+  header: {
+    fontSize: 20,
+    fontFamily: 'space-mono'
   },
   contentContainer: {
     paddingTop: 15,
